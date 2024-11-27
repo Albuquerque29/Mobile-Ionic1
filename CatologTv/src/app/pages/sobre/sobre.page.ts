e2f6c8e6fd8749be2e7cc67d9a638eb3e1a636ac
Albuquerque29 <arthurv.albuquerque@hotmail.com>
2024-11-27 19:40:42 -0300

    Up v1.1

--- CatologTv/src/app/pages/sobre/sobre.page.ts
+++ CatologTv/src/app/pages/sobre/sobre.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.page.html',
   styleUrls: ['./sobre.page.scss'],
 })
-export class SobrePage implements OnInit {
+export class SobrePage {
+  // Usamos HostBinding para alterar o estilo do elemento
+  @HostBinding('style.color') textColor: string = 'black'; // Cor inicial do texto
 
-  constructor() { }
-
-  ngOnInit() {
+  // Método para alterar a cor ao passar o mouse
+  @HostListener('mouseenter') onMouseEnter() {
+    this.textColor = 'blue'; // Mudar a cor para azul quando o mouse entra
   }
 
+  // Método para restaurar a cor ao sair o mouse
+  @HostListener('mouseleave') onMouseLeave() {
+    this.textColor = 'black'; // Restaurar a cor original
+  }
+
+  constructor() {}
 }
